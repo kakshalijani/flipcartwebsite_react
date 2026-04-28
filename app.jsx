1bcbc1238fbc685c35ffaebed79b3df9b4904e1f
kakshalijani <kakshalijani@gmail.com>
2026-04-28 14:22:59 +0530

    Add files via upload

---
+++ app.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Header from "./components/Header";
+import Login from "./components/Login";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Login />
+    </div>
+  );
+}
+
+export default App;
